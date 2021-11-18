5a474f83f31f3875e016c1271eb7732611ec25ea
jiangbo <563839592@qq.com>
2021-11-18 15:30:00 +0800

    判断activity是否为null

--- android/src/main/java/com/appsflyer/appsflyersdk/AppsflyerSdkPlugin.java
+++ android/src/main/java/com/appsflyer/appsflyersdk/AppsflyerSdkPlugin.java
@@ -88,12 +88,18 @@ public class AppsflyerSdkPlugin implements MethodCallHandler, FlutterPlugin, Act
         this.mContext = applicationContext;
         Intent intent = new Intent();
         intent.setClassName("com.appsflyer.appsflyersdk", "AppsflyerSdkPlugin");
-        if(intent.resolveActivity(activity.getPackageManager()) != null) {
-            // 说明系统中存在这个activity
-            this.mApplication = activity.getApplication();
-            this.mIntent = activity.getIntent();
-            onAttachedToEngine(applicationContext,messenger);
+        if (activity == null || activity.isDestroyed() || activity.isFinishing()) {
+
+
+        }else{
+            if(intent.resolveActivity(activity.getPackageManager()) != null) {
+                // 说明系统中存在这个activity
+                this.mApplication = activity.getApplication();
+                this.mIntent = activity.getIntent();
+                onAttachedToEngine(applicationContext,messenger);
+            }
         }
+
     }
 
 
